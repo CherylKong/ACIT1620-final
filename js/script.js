eb7f52c4e3c51572c09a547a61a69b477914817a
Cheryl Kong <ykong13@my.bcit.ca>
2020-12-09 12:39:36 -0800

    make form visible when mouse hower to the images

--- js/script.js
+++ js/script.js
@@ -2,17 +2,24 @@ let form = document.body.querySelector('.form')
 
 form.classList.add('visible')
 
+let image_div = document.querySelector('#thumb-bar')
 function add_image()
 {
     for (let i =1; i<=5;i++)
     {
         let img = document.createElement('img')
-        let image_div = document.querySelector('#thumb-bar')
         img.src = `images/pic${i}.jpg`
         img.alt = `image${i}`
-        img.setAttribute('height', '40');//img.height
-        img.setAttribute('width', '40');//img.width
+        img.setAttribute('height', '60');//img.height
+        img.setAttribute('width', '50');//img.width
         image_div.appendChild(img)
     }
 }
 add_image()
+
+function handlemouseover(e)
+{
+    form.classList.remove('visible')
+}
+
+image_div.addEventListener('mouseover',handlemouseover)
