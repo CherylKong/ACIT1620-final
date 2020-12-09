0adf99423335741f07fe1757ff2ce04a18deae4e
Cheryl Kong <ykong13@my.bcit.ca>
2020-12-09 12:27:24 -0800

    add loop to load the iamges

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 let form = document.body.querySelector('.form')
 
 form.classList.add('visible')
+
+function add_image()
+{
+    for (let i =1; i<=5;i++)
+    {
+        let img = document.createElement('img')
+        let image_div = document.querySelector('#thumb-bar')
+        img.src = `images/pic${i}.jpg`
+        img.alt = `image${i}`
+        img.setAttribute('height', '40');//img.height
+        img.setAttribute('width', '40');//img.width
+        image_div.appendChild(img)
+    }
+}
+add_image()
